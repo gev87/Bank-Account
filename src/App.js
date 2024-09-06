b10ba3148a4ccd26b10bd7ffe9551ed4e29ee477
Gevorg Petrosyan <armen.kharatyuan@menugroup.co.uk>
2024-09-06 11:53:11 +0400

    balanceDisplay component connected to redux in a legacy way, installed thunk middleWare and made http request in action creator function,installed @redux-devtools/extension package

--- src/App.js
+++ src/App.js
@@ -2,15 +2,24 @@ import BalanceDisplay from "./features/accounts/BalanceDisplay";
 import CreateCustomer from "./features/customers/CreateCustomer";
 import Customer from "./features/customers/Customer";
 import AccountOperations from "./features/accounts/AccountOperations";
+import { useSelector } from "react-redux";
 
 function App() {
+	const fullName = useSelector((state) => state.customer.fullName);
+	console.log("apprendered", fullName);
+
 	return (
 		<div>
 			<h1>🏦 The React-Redux Bank ⚛️</h1>
-			<CreateCustomer />
-			<Customer />
-			<AccountOperations />
-			<BalanceDisplay />
+			{fullName ? (
+				<>
+					<Customer />
+					<AccountOperations />
+					<BalanceDisplay />
+				</>
+			) : (
+				<CreateCustomer />
+			)}
 		</div>
 	);
 }
